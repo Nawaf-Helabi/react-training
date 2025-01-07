5a415aabc5d3af2211d77290e39ded35db276832
Nawaf Bin Helabi <nawafbinhelabi@gmail.com>
2025-01-07 21:30:59 +0300

    tutorial-14&15

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -7,16 +7,20 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
+  const [name, setName] = useState('mario');
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
-
-
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  }, [name])
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 }
