b25625ea101e1fd67fb5e0c29a37d9a4f01abc9c
Nawaf Bin Helabi <nawafbinhelabi@gmail.com>
2025-01-07 10:40:06 +0300

    tutorial-5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title='welcome to a new blog'
-  const likes=50
-  const person={name:'nawaf',age:25}
-  const link='www.google.com'
+
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>
-          {title}
-        </h1>
-        <p>liked {50} times</p>
-        <p>{person.name}</p>
-        <p>{10}</p>
-        <p>{'hello nawaf'}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <a  href={link}>google</a>
-        <p>{Math.random()*4}</p>
+       <Home/>
       </div>
     </div>
   );
