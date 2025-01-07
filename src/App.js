fbbb8f5ca9680e9688d9bb5de3b59b88841440b4
Nawaf Bin Helabi <nawafbinhelabi@gmail.com>
2025-01-07 10:03:04 +0300

    tutorial 4

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const title='welcome to a new blog'
+  const likes=50
+  const person={name:'nawaf',age:25}
+  const link='www.google.com'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code> and reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>
+          {title}
+        </h1>
+        <p>liked {50} times</p>
+        <p>{person.name}</p>
+        <p>{10}</p>
+        <p>{'hello nawaf'}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <a  href={link}>google</a>
+        <p>{Math.random()*4}</p>
+      </div>
     </div>
   );
 }
