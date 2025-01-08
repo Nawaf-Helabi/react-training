7fd9c82f14d2757ceb1d155dcd483ab77c03710c
Nawaf Bin Helabi <nawafbinhelabi@gmail.com>
2025-01-08 18:25:23 +0300

    tutorial-21&22&23&24&25

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  
-    
   return (
     <nav className="navbar">
-        <h1>
-            The Dojo Blog
-        </h1>
-        <div className='links'>
-           <a href="/">Home</a>
-           <a href="/creete">new blog</a>
-        </div>
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
+      </div>
     </nav>
-  )
+  );
 }
-
-export default Navbar
+ 
+export default Navbar;
